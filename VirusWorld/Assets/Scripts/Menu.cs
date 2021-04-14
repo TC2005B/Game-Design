ee1101bce51bec9cc3bc9b44521c186d1d8a9007
EdgarRostro <edgar_rostro@outlook.com>
2021-04-14 18:03:53 -0500

    14/abril

---
+++ VirusWorld/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void inicio()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Mapa");
+    }
+
+    public void opciones()
+    {
+        SceneManager.LoadScene("Opciones");
+    }
+}
---
+++ VirusWorld/Assets/Scripts/Ubicacion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ubicacion : MonoBehaviour
+{
+    [SerializeField] Transform [] ubicaciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = ubicaciones[Random.Range(0 ,ubicaciones.Length)].position;
+    }
+}
